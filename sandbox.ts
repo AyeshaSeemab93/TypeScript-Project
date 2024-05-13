cf548e53642b362455e0fc0d76d422332779bcc0
Ayesha Seemab <ayeshaseemab2017@gmail.com>
2024-05-13 14:09:09 +0300

    types in TS

--- sandbox.ts
+++ sandbox.ts
@@ -1,14 +1,19 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
+
 // character = 20;
 character = "luigi";
+
 // age = 'yoshi';
 age = 40;
+
 // isBlackBelt = 'yes';
 isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
 };
+
 // console.log(area('hello'));
 console.log(circ(7.5));
